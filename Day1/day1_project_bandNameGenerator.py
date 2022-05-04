dbf57ebeba46e6ef6f72c057a486f125ac93dbf4
siddhant.jha <siddhant.jha@tcs.com>
2022-05-04 17:35:14 +0530

    Final project for Day 1: Band Name Generator

---
+++ Day1/day1_project_bandNameGenerator.py
@@ -0,0 +1,19 @@
+#WAP to ask users about their birth city and pet name to generate an awesome band name for their upcoming rock band
+
+#Setting up an awesome greeting for the users
+print("Hey There, Welcome to the band name generator app\n" + "This app will generate some awesome name for your upcoming rock band")
+
+#Asking user for their name to add a personal touch
+print("Hey, " + input("But first things first, Help me out with your name\t") + " Thanks for sharing out your name, now let's get started")
+
+#Asking user for the name of their birth city and pet name
+cityName= input("What was the name of the city that you grew up in?\t")
+petName= input("What is the name of your pet\t")
+
+#Adding a pinch of extravegenza to spice things up
+print("Asking the magic ball to generate an awesome for your to be rock band")
+
+#Printing out the name of the rockband generated using string concatination and manipulation
+print("Magic ball has spoken, Name of your rock band should be: " + petName+" " +cityName)
+
+#This marks the end of Day 1 of the 100 Days of Python Bootcamp
