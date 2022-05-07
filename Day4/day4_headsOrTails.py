f1882f3d7ae7bcdb2500616d1f28a5ca7bbc610a
siddhant.jha <siddhant.jha@tcs.com>
2022-05-07 22:29:45 +0530

    Day 4: Practice (Not Completed)

---
+++ Day4/day4_headsOrTails.py
@@ -0,0 +1,15 @@
+#WAP to simulate a coin toss incorporating the use of random module
+#0 is a Head and 1 is a Tail
+
+#Importing the random module
+import random
+
+#Welcome prompt for the user
+print("Hey There, Welcome to the coin toss program")
+
+headOrTail = random.randint(0,1)    #This will generate a random number between 0 and 1
+
+if headOrTail == 0:
+    print("Head's")
+else:
+    print("Tails")
