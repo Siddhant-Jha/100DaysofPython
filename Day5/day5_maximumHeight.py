681417710500473ae4b18fbe820a1e636f42d7aa
siddhant.jha <siddhant.jha@tcs.com>
2022-05-11 17:55:43 +0530

    Completed the maximum height problem

--- Day5/day5_maximumHeight.py
+++ Day5/day5_maximumHeight.py
@@ -3,13 +3,12 @@
 #Welcome Prompt for the user
 print("Hey There, Welcome to the Max Height calculator app, This app will find the maximum height from a list of height's without using max() function")
 
-listOfHeights = input("Please enter the list of height's separated by spaces").split(" ")
+listOfHeights = input("Please enter the list of height's separated by spaces\t").split(" ")
 
 maximumHeight = 0
 
 for height in listOfHeights:
-    if maximumHeight > float(height):
+    if float(height) > maximumHeight:
         maximumHeight = float(height)
-        print(f"The maximum height is {maximumHeight}")
-    else:
-        print(f"The maximum lenght is {height}")
+
+print(f"The maximum height is {maximumHeight}")
