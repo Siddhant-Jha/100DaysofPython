ef539166bed6fcae4f84f5b6a84aefa857d7e0cb
Siddhant Jha <60434913+Siddhant-Jha@users.noreply.github.com>
2022-05-13 18:41:32 +0530

    Update day6_challange1.py

--- Day6/day6_challange1.py
+++ Day6/day6_challange1.py
@@ -1,3 +1,4 @@
+#Day6 Challange 1
 #Challange Link
 #https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
 
