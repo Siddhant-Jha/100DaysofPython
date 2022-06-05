717f3664f35eaeaa3945bd1f46412fd6ef09cdf5
Siddhant Jha <60434913+Siddhant-Jha@users.noreply.github.com>
2022-06-05 17:06:47 +0530

    Update day19_snakeClass.py

--- Day19/day19_snakeClass.py
+++ Day19/day19_snakeClass.py
@@ -20,7 +20,24 @@ class CreateSnake:
         self.left()
         self.right()
 
+    
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+        
+        
     #Creating Snake
     def createSnake(self):
         for postion in STARTING_POSITION:
@@ -37,19 +54,3 @@ class CreateSnake:
             self.segments[segmentNumber].goto(x=xCorOfPreviousSegment,y=yCorOfPreviousSegment)
 
         self.head.forward(MOVE_DISTANCE)
-
-    def up(self):
-        if self.head.heading() != DOWN:
-            self.head.setheading(UP)
-
-    def down(self):
-        if self.head.heading() != UP:
-            self.head.setheading(DOWN)
-
-    def left(self):
-        if self.head.heading() != RIGHT:
-            self.head.setheading(LEFT)
-
-    def right(self):
-        if self.head.heading() != LEFT:
-            self.head.setheading(RIGHT)
